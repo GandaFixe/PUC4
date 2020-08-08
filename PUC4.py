e1b1fec978d43ce29356b07ff7da74105f966957
GandaFixe <59183743+GandaFixe@users.noreply.github.com>
2020-08-09 00:40:05 +0100

    Loading cogs system

--- PUC4.py
+++ PUC4.py
@@ -191,7 +191,19 @@ async def server(ctx):
     await ctx.send(f'Region: {region}')
     await ctx.send(f'Number of members: {members}')
 
+# Commands to load cogs
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send(f'Sucessfully loaded {extension}')
+    
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    await ctx.send(f'Sucessfully unloaded {extension}')
 
-
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run('TOKEN HERE')

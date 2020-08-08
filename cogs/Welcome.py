808514f1cf9d27fe8c89fa477c7d0f31bacccba5
GandaFixe <59183743+GandaFixe@users.noreply.github.com>
2020-08-09 00:48:36 +0100

    Cog

--- cogs/Welcome.py
+++ cogs/Welcome.py
@@ -5,14 +5,18 @@ from discord.ext.commands import has_permissions, CheckFailure
 import os
 import json
 
+
+# Setup working function 
 def setup(client):
     client.add_cog(Welcome(client))
 
+# Official class for the Welcome.py
 class Welcome(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
 
+    # Cog event to welcome mebers to the server
     @commands.Cog.listener()
     async def on_member_join(self, member):
         channel= member.guild.system_channel
